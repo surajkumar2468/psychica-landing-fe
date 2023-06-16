bcf28d8c66d689382e26f51505aa2df32d1d1ce7
TechnicalArshad <arshad.khan@seekware.in>
2023-06-16 13:48:55 +0530

    issue resolve

--- src/page/professionallists/index.jsx
+++ src/page/professionallists/index.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom"; 
+import { useLocation } from "react-router-dom";
 import PsychicListData from "../../components/PsychicListData";
 import axiosInstance from "../../config/axiosinstance";
 import { API_URLS } from "../../utils/API_URLS";
-import { useNavigate } from 'react-router-dom';
-
+import { useNavigate } from "react-router-dom";
+import { AiOutlineCheck } from "react-icons/ai";
 
 const ProfessionalLists = () => {
   const navigate = useNavigate();
@@ -16,21 +16,28 @@ const ProfessionalLists = () => {
   const location = useLocation();
 
   const queryParams = new URLSearchParams(location.search);
+  let heading = queryParams
+  console.log("QUERYPARAMS",heading);
 
   const getProfessionalByType = () => {
-    axiosInstance.get(`${API_URLS.psychicCategory}type?type=${queryParams.get('type')}&value=${queryParams.get('value').toLowerCase()}`)
+    axiosInstance
+      .get(
+        `${API_URLS.psychicCategory}type?type=${queryParams.get(
+          "type"
+        )}&value=${queryParams.get("value").toLowerCase()}`
+      )
       .then((res) => {
         console.log("RES", res);
         if (res) {
           setRecommendedPsychic(res[0]);
           setProfessionalList(res.slice(1, 7));
         }
-      })
-  }
+      });
+  };
 
   useEffect(() => {
-    getProfessionalByType()
-  }, [])
+    getProfessionalByType();
+  }, []);
 
   const handleBookappointment = (id, picture, price, type, name, timezone) => {
     const queryParams = {
@@ -38,106 +45,154 @@ const ProfessionalLists = () => {
       price: price,
       type: type,
       name: name,
-      timezone: timezone
+      timezone: timezone,
     };
 
     navigate({
       pathname: `/appointment-now/${id}`,
       search: new URLSearchParams(queryParams).toString(),
     });
-  }
+  };
 
-
+  const typeName = queryParams.get("type") == "READING_TOPIC" ? "Reading Topic" : queryParams.get("type")
+  const ren = typeName.toLowerCase();
+  // const ren = typeName.charAt(0).toUpperCase() + typeName.slice(1);
+  
   return (
     <div>
-      {
-        recommendedPsychic ?
-          <div className="home_sec3">
-            <section className='explore_category_sec'>
-              <div className='container'>
-                <h2>{`Explore ${queryParams.get('type')} Psychics`}</h2>
-                <div className='row'>
-                  <div className='col-12 col-md-7'>
-                    <div className='category_hedding'><h5>{recommendedPsychic?.first_name}</h5>
-                      <p>
-                        <img src={"/images/abilities-icon.svg"} alt="" />
-                        {
-                          recommendedPsychic?.abilities?.map((ability, idx) => {
-                            return (
-                              <span>
-                                {idx !== 0 && ", "}
-                                {ability?.charAt(0).toUpperCase() + ability.slice(1)}
-                              </span>
-                            )
-                          })
-                        }
-                      </p>
-                    </div>
-                    <div className='img_explore_sec'>
-                      <img src={recommendedPsychic?.picture} alt="" />
-                      <p><img src="/images/check_icon.svg" alt="" />
-                        Recommended By Staff</p>
-                    </div>
-                  </div>
+      {recommendedPsychic ? (
+        <div className="home_sec3">
+          <section className="explore_category_sec">
+            <div className="container">
+             <h2>Explore  {ren} psychics</h2>
 
-                  <div className='col-12 col-md-5'>
-                    <div className='category__right_main'>
-                      <div className='category__right_row1'>
-                        <div>
-                          <h3> ${`${recommendedPsychic?.actual_rate}`} <small>/minute</small></h3>
-                          <div className='star__rating'><img src="/images/star-icon.svg" alt="" /> {`${recommendedPsychic?.average_rating}`} <i>{`(${recommendedPsychic?.review_count} reviews)`}</i></div>
+              {/* <h2 classNam="typeHeading">{`Explore ${queryParams.get("type")} Psychics`}</h2> */}
+              <div className="row">
+                <div className="col-12 col-md-7">
+                  <div className="category_hedding">
+                    <h5>{recommendedPsychic?.first_name}</h5>
+                    <p>
+                      <img src={"/images/abilities-icon.svg"} alt="" />
+                      {recommendedPsychic?.abilities?.map((ability, idx) => {
+                        return (
+                          <span>
+                            {idx !== 0 && ", "}
+                            {ability?.charAt(0).toUpperCase() +
+                              ability.slice(1)}
+                          </span>
+                        );
+                      })}
+                    </p>
+                  </div>
+                  <div className="img_explore_sec">
+                    <img
+                      src={recommendedPsychic?.picture}
+                      alt=""
+                      classNam="img-user"
+                    />
+                    <p>
+                      {/* <AiOutlineCheck className="check_icon"/> */}
+                      <img
+                        src="/images/check_icon.svg"
+                        alt=""
+                        classNam="img-icon"
+                      />
+                      Recommended By Staff
+                    </p>
+                  </div>
+                </div>
+
+                <div className="col-12 col-md-5">
+                  <div className="category__right_main">
+                    <div className="category__right_row1">
+                      <div>
+                        <h3>
+                          {" "}
+                          ${`${recommendedPsychic?.actual_rate}`}{" "}
+                          <small>/minute</small>
+                        </h3>
+                        <div className="star__rating">
+                          <img src="/images/star-icon.svg" alt="" />{" "}
+                          {`${recommendedPsychic?.average_rating}`}{" "}
+                          <i>{`(${recommendedPsychic?.review_count} reviews)`}</i>
                         </div>
-                        <div className='star__img'>
-                          <img src="/images/pexels-pavel-danilyuk2.png" alt="" />
-                          <span><img src="/images/check_green.svg" alt="" /></span>
-                        </div>
                       </div>
-                      <div className='category__right_row2'>
-                        <ul>
-                          <li>
-                            <span><img src="/images/status_icon.svg" alt="" /></span>
-                            <h4>Status <b>Available</b></h4>
-                          </li>
-                          <li>
-                            <span><img src="/images/status_icon.svg" alt="" /></span>
-                            <h4>Estimated Wait <b>0 minutes</b></h4>
-                          </li>
-                          <li>
-                            <span><img src="/images/man_icon.svg" alt="" /></span>
-                            <h4>Time Minimum <b>30 minutes</b></h4>
-                          </li>
-                        </ul>
+                      <div className="star__img">
+                        <img src="/images/pexels-pavel-danilyuk2.png" alt="" />
+                        <span>
+                          <img src="/images/check_green.svg" alt="" />
+                        </span>
                       </div>
-                      <div className='category__right_row3'>
-                        <ul>
-                          <li>
-                            <a href=''>Learn About {recommendedPsychic?.first_name}</a>
-                          </li>
-                          <li>
-                            <a
-                              // href={`/appointment-now/${recommendedPsychic?.id}/${recommendedPsychic?.picture}`}
-                              onClick={()=>handleBookappointment(recommendedPsychic?.id, recommendedPsychic?.picture, recommendedPsychic?.price, recommendedPsychic?.type, recommendedPsychic?.first_name, recommendedPsychic?.timezone)}
-                            >
-                              Meet</a>
-                          </li>
-                        </ul>
-                        <p className='right_row3_tag'>
-                          <img src="/images/report_icon.svg" alt="" />
-                          Report this psychic
-                        </p>
-                      </div>
+                    </div>
+                    <div className="category__right_row2">
+                      <ul>
+                        <li>
+                          <span>
+                            <img src="/images/status_icon.svg" alt="" />
+                          </span>
+                          <h4>
+                            Status <b>Available</b>
+                          </h4>
+                        </li>
+                        <li>
+                          <span>
+                            <img src="/images/status_icon.svg" alt="" />
+                          </span>
+                          <h4>
+                            Estimated Wait <b>0 minutes</b>
+                          </h4>
+                        </li>
+                        <li>
+                          <span>
+                            <img src="/images/man_icon.svg" alt="" />
+                          </span>
+                          <h4>
+                            Time Minimum <b>30 minutes</b>
+                          </h4>
+                        </li>
+                      </ul>
+                    </div>
+                    <div className="category__right_row3">
+                      <ul>
+                        <li>
+                          <a href="">
+                            Learn About {recommendedPsychic?.first_name}
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            // href={`/appointment-now/${recommendedPsychic?.id}/${recommendedPsychic?.picture}`}
+                            onClick={() =>
+                              handleBookappointment(
+                                recommendedPsychic?.id,
+                                recommendedPsychic?.picture,
+                                recommendedPsychic?.price,
+                                recommendedPsychic?.type,
+                                recommendedPsychic?.first_name,
+                                recommendedPsychic?.timezone
+                              )
+                            }
+                          >
+                            Meet
+                          </a>
+                        </li>
+                      </ul>
+                      <p className="right_row3_tag">
+                        <img src="/images/report_icon.svg" alt="" />
+                        Report this psychic
+                      </p>
                     </div>
                   </div>
                 </div>
               </div>
-            </section>
-            <PsychicListData professionalList={professionalList} />
-          </div>
-          :
-          <h3>No Data Available</h3>
-      }
+            </div>
+          </section>
+          <PsychicListData professionalList={professionalList} />
+        </div>
+      ) : (
+        <h3>No Data Available</h3>
+      )}
     </div>
-
   );
 };
 
