45b2c39e34bb0e54420c7eebee43302d11a49846
surajkumar2468 <suraj.kanth@seekware.in>
2023-06-16 20:57:59 -0600

    Linked integrated

--- src/page/forpsychics/index.js
+++ src/page/forpsychics/index.js
@@ -1,81 +1,121 @@
-import React from 'react'
+import React from "react";
 
 const index = () => {
-    return (
-        <>
-            <div className='container'>
-                <section className='under_development_sec'>
-                    <div className='development_sec_main'>
-                        <div className='development_sec_content'>
-                            <h2>No other psychic
-                                platform compares.</h2>
-                            <p>When we say we’re the best psychic platform around, it’s not just a marketing gimmick. No platform even comes close to our feature set for both clients or psychics. See the difference for yourself.</p>
-                            <a href='' className='btn_under'>Experience a better platform  </a>
-                        </div>
-                        <div className='development_sec_img'>
-                            <img src="/images/card_compares_img.png" alt="" />
-                        </div>
-                    </div>
-                </section>
-                <section className='home_sec5 under_development_sec2'>
-                    <span><img src="/images/card_under_img1.jpg" alt="" /></span>
-                    <h2>Enjoy a full suite of tools
-                        <abbr> to help you expand your psychic business.</abbr></h2>
-                    <p>Leveraging our cutting-edge video conferencing technology and pioneering AI-powered solutions, we set the bar high in delivering the most comprehensive suite of psychic tools. Discover the distinctive edge that not only offers more cost-effective options than rival platforms but also promises an unparalleled user experience.
-                    </p>
-                </section>
-                <section className='under_development_sec3'>
-                    <div className='development_sec_content3'>
-                        <h2>Unlike those other marketplaces,
-                            we simply treat our psychics
-                            with love and respect.</h2>
-                        <p>When Kevin started using competing platforms,
-                            he realized how badly psychics were treated and how
-                            other platforms were running their service providers
-                            over with insane rates. Kevin set out to change that
-                            and build an entirely new platform that simply put the
-                            others to bed.</p>
-                        <ul>
-                            <li>
-                                <a href=''>Read Kevin’s story</a>
-                            </li>
-                            <li>
-                                <a href=''>Signup as psychic</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className='development_sec_img3'>
-                        <img src="/images/vector-sec_img2.png" alt="" />
-                    </div>
-                </section>
-                <section className='under_development_sec4'>
-                    <span><img src="/images/rectangle_under_img.png" alt="" /></span>
-                    <div className='under_development_sec4_content'>
-                        <h2>Instant bank payouts
-                            <abbr>and your very own debit card,</abbr>
-                            so you can spend earnings instantly.</h2>
-                        <p>From instant payouts to a branded debit card,
-                            Psychics </p>
-                        <a href='' className='btn_sec4_content'>Signup as a psychic</a>
-                    </div>
-                </section>
-                <section className='under_development_sec3 under_development_sec5'>
-                    <div className='development_sec_content3'>
-                        <h2>It’s doesn’t take a psychic to see the difference.
-                            <span> Join the many other psychics who have made the switch.</span></h2>
-                        <ul>
-                            <li>
-                                <a href=''>Signup as a psychic</a>
-                            </li>
-                            <li>
-                                <a href=''>See psychic features</a>
-                            </li>
-                        </ul>
-                    </div>
-                </section>
+  return (
+    <>
+      <div className="container">
+        <section className="under_development_sec">
+          <div className="development_sec_main">
+            <div className="development_sec_content">
+              <h2>No other psychic platform compares.</h2>
+              <p>
+                When we say we’re the best psychic platform around, it’s not
+                just a marketing gimmick. No platform even comes close to our
+                feature set for both clients or psychics. See the difference for
+                yourself.
+              </p>
+              <a
+                href="https://psychica.seekware.tech/client/signup"
+                className="btn_under"
+              >
+                Experience a better platform{" "}
+              </a>
             </div>
-        </>
-    )
-}
+            <div className="development_sec_img">
+              <img src="/images/card_compares_img.png" alt="" />
+            </div>
+          </div>
+        </section>
+        <section className="home_sec5 under_development_sec2">
+          <span>
+            <img src="/images/card_under_img1.jpg" alt="" />
+          </span>
+          <h2>
+            Enjoy a full suite of tools
+            <abbr> to help you expand your psychic business.</abbr>
+          </h2>
+          <p>
+            Leveraging our cutting-edge video conferencing technology and
+            pioneering AI-powered solutions, we set the bar high in delivering
+            the most comprehensive suite of psychic tools. Discover the
+            distinctive edge that not only offers more cost-effective options
+            than rival platforms but also promises an unparalleled user
+            experience.
+          </p>
+        </section>
+        <section className="under_development_sec3">
+          <div className="development_sec_content3">
+            <h2>
+              Unlike those other marketplaces, we simply treat our psychics with
+              love and respect.
+            </h2>
+            <p>
+              When Kevin started using competing platforms, he realized how
+              badly psychics were treated and how other platforms were running
+              their service providers over with insane rates. Kevin set out to
+              change that and build an entirely new platform that simply put the
+              others to bed.
+            </p>
+            <ul>
+              <li>
+                <a href="">Read Kevin’s story</a>
+              </li>
+              <li>
+                <a href="https://psychica.seekware.tech/professional/signup">
+                  Signup as psychic
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="development_sec_img3">
+            <img src="/images/vector-sec_img2.png" alt="" />
+          </div>
+        </section>
+        <section className="under_development_sec4">
+          <span>
+            <img src="/images/rectangle_under_img.png" alt="" />
+          </span>
+          <div className="under_development_sec4_content">
+            <h2>
+              Instant bank payouts
+              <abbr>and your very own debit card,</abbr>
+              so you can spend earnings instantly.
+            </h2>
+            <p>From instant payouts to a branded debit card, Psychics </p>
+            <a
+              href="https://psychica.seekware.tech/professional/signup"
+              className="btn_sec4_content"
+            >
+              Signup as a psychic
+            </a>
+          </div>
+        </section>
+        <section className="under_development_sec3 under_development_sec5">
+          <div className="development_sec_content3">
+            <h2>
+              It’s doesn’t take a psychic to see the difference.
+              <span>
+                {" "}
+                Join the many other psychics who have made the switch.
+              </span>
+            </h2>
+            <ul>
+              <li>
+                <a href="https://psychica.seekware.tech/client/signup">
+                  Signup as a psychic
+                </a>
+              </li>
+              <li>
+                <a href="https://psychica.seekware.tech/client/signup">
+                  See psychic features
+                </a>
+              </li>
+            </ul>
+          </div>
+        </section>
+      </div>
+    </>
+  );
+};
 
-export default index
+export default index;
